d818ba244cdadfd9cae5218ef79f0fbad9aefadc
yuri-silveiraa <yuri.pires.silveira@gmail.com>
2021-12-12 15:02:21 -0300

    final version

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+  loop:false,
+  margin:12,
+  nav:false,
+  responsive:{
+      0:{
+          items:2
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:5
+      }
+  }
+})
